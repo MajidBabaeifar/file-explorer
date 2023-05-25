37971ca88388b3b4b2e4ede75ad35c9b9a061e77
Mj Dev <majidbabaeifar@gamil.com>
2023-05-25 19:10:59 +0330

    fixed image select too

--- src/components/directoryView/thumbnails/ImageThumbnail.tsx
+++ src/components/directoryView/thumbnails/ImageThumbnail.tsx
@@ -1,15 +1,43 @@
 import React, { useState } from "react";
 import ModalWrapper from "../../common/modalwrapper/ModalWrapper";
 import { BsCardImage } from "react-icons/bs";
+import {
+  getFiles,
+  manipulateDirectory,
+} from "../../../store/feature/files/files";
+import { useDispatch, useSelector } from "react-redux";
+import { fileAction } from "../../../store/feature/files/file.actions";
 
 const ImageThumbnail = ({ file }) => {
   const [showModal, setShowModal] = useState(false);
+  const dispatch = useDispatch();
+  const { selectedFiles } = useSelector(getFiles);
+
+  function handleClick(e) {
+    e.preventDefault();
+    if (e.type === "click") {
+      setShowModal(true);
+    } else if (e.type === "contextmenu") {
+      dispatch(
+        manipulateDirectory({
+          type: fileAction.toggleSelectFiles,
+          data: file?.id,
+        })
+      );
+    }
+  }
   return (
     <div>
       <ModalWrapper showModal={showModal} setShowModal={setShowModal}>
         <img className="image-modal" src={file?.url} alt={file?.title} />
       </ModalWrapper>
-      <div onClick={() => setShowModal(true)} className="m-2 file">
+      <div
+        onContextMenu={handleClick}
+        onClick={handleClick}
+        className={`m-2 file ${
+          selectedFiles.includes(file.id) ? "selectedFile" : ""
+        }`}
+      >
         <BsCardImage size={56} />
         <div>{file.title}</div>
       </div>
